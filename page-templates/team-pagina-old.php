9025764f6b323afae475c89972f09b54bcedf6a1
ntenhoedt <Nikos@1.local>
2015-12-03 14:17:13 +0100

    nieuwe teampagina

--- page-templates/team-pagina-old.php
+++ page-templates/team-pagina-old.php
@@ -20,51 +20,32 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
-            
 			<?php
 				// Start the Loop.
 				while ( have_posts() ) : the_post();
 			?>
 
-            
-            
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<?php
 					the_title( '<header class="entry-header"><h1 class="entry-title">', '</h1></header><!-- .entry-header -->' );
 
+					//Toon alle Hoofdredacteuren
+						$blogusers = get_users( 'orderby=nicename&role=editor' );
+							// Array of WP_User objects.
+							foreach ( $blogusers as $user ) {
+								echo '<span>' . esc_html( $user->display_name ) . '</span>';
+							}
+							
+					//Toon alle Redacteuren
+						$blogusers = get_users( 'orderby=nicename&role=author' );
+							// Array of WP_User objects.
+							foreach ( $blogusers as $user ) {
+								echo '<span>' . esc_html( $user->display_name ) . '</span>';
+							}
+
+
+
 				?>
-                
-                <div class="entry-content">
-            <div class="teamcontent">    
-               
-    
-    
-  
-                
-                <?php
-
-// The Query
-$user_query = new WP_User_Query( array( 'role' => 'beeldmaker' ) );
-
-// User Loop
-if ( ! empty( $user_query->results ) ) {
-	foreach ( $user_query->results as $user ) {
-		echo '<p>' . $user->display_name . '</p>';
-	}
-} else {
-	echo 'No users found.';
-}
-
-wp_list_authors('show_fullname=1&optioncount=1&orderby=post_count&order=DESC&number=300');
-
-wp_list_authors('show_fullname=1&optioncount=1&orderby=post_count&order=DESC&number=300'); 
-                ?>
- 
-        
-                </div>
-                    </div>
-                    </div>
-                
 			</article><!-- #post-## -->
 
 			<?php
@@ -77,5 +58,3 @@ wp_list_authors('show_fullname=1&optioncount=1&orderby=post_count&order=DESC&num
 <?php
 get_sidebar();
 get_footer();
-
-
