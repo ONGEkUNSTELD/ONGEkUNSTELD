6dfa2ab7988642f47a0512944cf2769995ee3e57
QbpNogCYUGmaGPzD <dennisneumann@galmoer.nl>
2016-03-04 14:18:46 +0100

    Fix ontbreken users?
    Test

--- page-templates/team-pagina.php
+++ page-templates/team-pagina.php
@@ -37,7 +37,6 @@ get_header(); ?>
                 <div class="entry-content">
 					<div class="teamcontent">   
                         
-                        <p>Er is een schrijversteam van om en nabij 33 actieve schrijvers en een beeldmakersteam van inmiddels 24 vaste beeldmakers. Verder is iedereen vrij om een account aan te maken en artikelen in te sturen voor publicatie. Sporadisch treden gastschrijvers en gastbeeldmakers op. Onze samenstelling wisselt, toch is niemand inwisselbaar.</p> 
                         
 				        <div class="teamcolumn">
                            
